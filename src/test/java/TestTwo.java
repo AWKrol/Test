fc6e1575894a28a78b6a9259d1ffeeb56d4838a1
AWKrol <92876406+AWKrol@users.noreply.github.com>
2022-01-17 14:08:41 +0300

    commit1

---
+++ src/test/java/TestTwo.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestTwo {
+
+    @Test
+    public void testOne() {
+        Assert.assertEquals((2 + 2), 4);
+    }
+}
